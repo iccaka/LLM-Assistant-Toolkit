1fd1bfbbdd3d42a8d5bbc460aecff2f079137647
Hristo 'iccaka' Mitsev <dakillaroshun@gmail.com>
2025-06-14 00:20:38 +0300

    Modify main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import logging
 import httpx
+from langchain_community.document_loaders import DirectoryLoader
 
 EXIT_WORD = 'bye'
 FASTAPI_BASE_URL = 'http://127.0.0.1:8000'
-
+DEFAULT_TIMEOUT = 120.0
 
 def chat_with_llm():
     print('====================\n(Use \'{}\' to exit.)\n -> *[LLM Chat Mode]*'.format(EXIT_WORD))
@@ -15,7 +16,8 @@ def chat_with_llm():
             if user_input.lower() == EXIT_WORD:
                 break
             else:
-                llm_response = httpx.post(FASTAPI_BASE_URL + '/chat', timeout=60.0, json={'message': user_input})
+                llm_response = httpx.post(FASTAPI_BASE_URL + '/chat', timeout=DEFAULT_TIMEOUT, json={'message': user_input})
+
                 print('LLM: {}'.format(llm_response.json()['reply']))
         except ValueError:
             print('Invalid input, please try again.')
@@ -26,11 +28,14 @@ def clean_document():
     print('====================\n(Use \'{}\' to exit.)\n -> *[Document Clean Mode]*'.format(EXIT_WORD))
     while True:
         try:
-            user_input = input('\nPath to document to clean: ')
+            user_input = input('\nDocument\'s name: ')
 
             if user_input.lower() == EXIT_WORD:
                 break
             else:
+                loader = DirectoryLoader("./sample_texts/{}".format(user_input), glob="*.txt")
+                print(len(loader.load()))
+                # TODO request
                 llm_response = 'test'
                 print('Cleaned document: {}'.format(llm_response))
         except ValueError:
