413c63b894d06b69a6e6013e962570770da3ad1d
Hristo 'iccaka' Mitsev <dakillaroshun@gmail.com>
2025-06-17 02:48:05 +0300

    WIP - Modify main.py

--- main.py
+++ main.py
@@ -17,11 +17,21 @@ Requires:
 
 import logging
 import httpx
+from langchain_community.document_loaders import TextLoader
+from mistral_common.tokens.tokenizers.mistral import MistralTokenizer
+
 
 EXIT_WORD = 'bye'
 FASTAPI_BASE_URL = 'http://127.0.0.1:8000'
 DEFAULT_TIMEOUT = 120.0
 DEFAULT_TEXTS_DIR = './sample_texts'
+DEFAULT_TOKENIZER = 'mistralai/Mistral-7B-Instruct-v0.1'
+DEFAULT_MISTRAL_CONTEXT_WINDOW = 8192
+
+max_context_tokens = 8192
+expected_output_tokens = 1000
+max_input_tokens = max_context_tokens - expected_output_tokens
+tokenizer = MistralTokenizer.v1()
 
 def chat_with_llm():
     """
@@ -70,18 +80,74 @@ def clean_document():
             if user_input.lower() == EXIT_WORD:
                 break
             else:
+                text = ''
                 try:
-                    with open('{}/{}'.format(DEFAULT_TEXTS_DIR, user_input), 'r', encoding='utf-8') as file:
-                        text = file.read()
-                        llm_response = httpx.post('{}/clean'.format(FASTAPI_BASE_URL), timeout=DEFAULT_TIMEOUT, json={'message': text})
-
-                        print('Cleaned document: {}'.format(llm_response.json()['reply']))
+                    loader = TextLoader('{}/{}'.format(DEFAULT_TEXTS_DIR, user_input))
+                    docs = loader.load()
+                    text = docs[0].page_content
                 except FileNotFoundError:
-                    print('File not found.')
-                    continue
+                    print("File not found.")
+                except UnicodeDecodeError:
+                    print("Encoding issue when reading the file.")
+                except Exception as e:
+                    print('Other error: {}'.format(type(e).__name__))
+
+                if __does_need_chunking(text, DEFAULT_MISTRAL_CONTEXT_WINDOW):
+                    chunks = __chunk_text(text, max_input_tokens)
+                    cleaned = [__llm_text_clean(chunk) for chunk in chunks]
+
+                    llm_response = ' '.join(cleaned)
+                else:
+                    llm_response = __llm_text_clean(text)
+
+                print('Cleaned document: {}'.format(llm_response))
         except ValueError:
             print('Invalid input, please try again.')
             continue
+
+
+def __count_tokens(text):
+    return len(tokenizer.encode(text))
+
+
+def __chunk_text(text, max_tokens):
+    words = text.split()
+    chunks = []
+    current_chunk = []
+
+    for word in words:
+        current_chunk.append(word)
+        token_len = __count_tokens(' '.join(current_chunk))
+
+        if token_len > max_tokens:
+            current_chunk.pop()
+            chunks.append(' '.join(current_chunk))
+            current_chunk = [word]
+
+    if current_chunk:
+        chunks.append(' '.join(current_chunk))
+
+    return chunks
+
+
+def __llm_text_clean(text):
+    llm_response =  httpx.post(
+        '{}/clean'.format(FASTAPI_BASE_URL),
+        timeout=DEFAULT_TIMEOUT,
+        json={'message': text}
+    )
+
+    return llm_response.json()['reply']
+
+
+def __does_need_chunking(text, context_window):
+    token_count = len(tokenizer.encode_chat_completion(text))
+    expected_output = int(0.8 * token_count)
+
+    if token_count + expected_output > context_window:
+        return True
+
+    return False
 
 
 def __return_to_selection():
